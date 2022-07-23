8ef3b15d91c59562471a17bec96e7d4b2b44e74d
NalishJain <nalish21543@iiitd.ac.in>
2022-07-23 16:14:32 +0530

    Added Kartiks function

--- SimpleSimulator.py
+++ SimpleSimulator.py
@@ -24,7 +24,11 @@ PC = 0
 halted = False
 
 
+
 def execute_typeA(Instruction):
+
+    rdict["111"] = "0000"
+
     if  Instruction[0:5] == "10000":
         resA = rdict[Instruction[7:10]] + rdict[Instruction[10:13]] 
         if resA < (2**16):
@@ -57,18 +61,50 @@ def execute_typeA(Instruction):
         resA = (rdict[Instruction[7:10]])&(rdict[Instruction[10:13]]) 
         rdict[Instruction[13:16]] = resA 
 
+def execute_typeB(Instruction):
+    # reset flags
+    rdict["111"] = "0000"
+    reg = Instruction[5:8]
+    imm = int(Instruction[8:], 2)
+    inst = Instruction[0:5]
+    if inst == "11000":
+        rdict[reg] = rdict[reg] >> imm
+    elif inst == "11001":
+        rdict[reg] = rdict[reg] << imm
+    elif inst == "10010":
+        rdict[reg] = imm
+
+def execute_typeC(Instruction):
+    # reset flags
+    rdict["111"] = "0000"
+    reg1 = Instruction[10:13]
+    reg2 = Instruction[13:16]
+    inst = Instruction[0:5]
+    if inst == "10011":
+        rdict[reg2] = rdict[reg1]
+    elif inst == "10111":
+        rdict["000"] = int(rdict[reg1] / rdict[reg2])
+        rdict["001"] = rdict[reg1] % rdict[reg2]
+    elif inst == "11101":
+        rdict[reg2] = ~rdict[reg1]
+    elif inst == "11110":
+        if rdict[reg1] < rdict[reg2]:
+            rdict["111"][-3] = 1
+        elif rdict[reg1] > rdict[reg2]:
+            rdict["111"][-2] = 1
+        elif rdict[reg1] == rdict[reg2]:
+            rdict["111"][-1] = 1
 
 def ExecuteInstruction(Instruction):
     if Instruction[0:5] in ["10000","10001","10110","11010" ,"11011","11100"]:
-        rdict["111"] = "0000"
         execute_typeA(Instruction)
-    elif Instruction[0:5] in ["11000", "11001"]:
-        # TypeB
-    elif Instruction[0:5] in ["10111", "11101", "11110"]:
-        # TypeC
-    elif Instruction[0:5] in ["01111", "01101", "11111", "01100"]:
+    elif Instruction[0:5] in ["11000","11001", "10010"]:
+        execute_typeB(Instruction)
+    elif Instruction[0:5] in ["10011","10111", "11101", "11110"]:
+        execute_typeC(Instruction)
+    elif Instruction[0:5] in ["10101", "10100"]:
         # TypeD
-    elif Instruction[0:5] in ["11000", "11001"]:
+    elif Instruction[0:5] in ["01111", "01101", "11111", "01100"]:
         # TypeE
     elif Instruction[0:5] == "01010":
         global halted = True
